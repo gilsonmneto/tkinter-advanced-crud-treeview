250cca47b2ad25b230fdf54514bd27d368ff79d7
gilsonmneto <34845199+gilsonmneto@users.noreply.github.com>
2020-04-01 16:52:41 -0300

    changes

--- github-start.bat
+++
@@ -1,6 +0,0 @@
-git init
-git add .
-git commit -m "first commit"
-for %%I in (.) do set CurrDirName=%%~nxI
-git remote add origin https://github.com/gilsonmneto/%CurrDirName%
-git push -u origin master
