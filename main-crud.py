a0e81ec9959d09efe67d22efbe1c753e5b6d819f
gilsonmneto <34845199+gilsonmneto@users.noreply.github.com>
2020-04-04 02:11:52 -0300

    update and delete.py

--- main-crud.py
+++ main-crud.py
@@ -6,7 +6,7 @@ import sqlite3
 from tkinter import messagebox
 
 
-class MainApplication():
+class MainApplication:
     """Create Application"""
 
     def __init__(self, window):
@@ -24,7 +24,8 @@ class MainApplication():
         # ID textbox
         tk.Label(self.frame, text='Code: ').grid(sticky=tk.W, row=1, column=0)
         self.code_var = tk.StringVar()
-        self.code = tk.Entry(self.frame, width=7, state='disabled', textvariable=self.code_var).grid(sticky=tk.W, row=1, column=1)
+        self.code = tk.Entry(self.frame, width=7, state='disabled', textvariable=self.code_var).grid(sticky=tk.W,
+                                                                                                     row=1, column=1)
 
         # Name textbox
         tk.Label(self.frame, text='Name: ').grid(sticky=tk.W, row=2, column=0)
@@ -47,13 +48,18 @@ class MainApplication():
         self.email = tk.Entry(self.frame, width=26, textvariable=self.email_var).grid(row=5, column=1)
 
         # Buttons
-        self.create = tk.Button(self.frame, text='Create', command=self.create, width=10).grid(row=6, column=1, sticky=tk.W)
-        self.update = tk.Button(self.frame, text='Update', width=10).grid(row=6, column=1, sticky=tk.E)
-        self.delete = tk.Button(self.frame, text='Delete', width=10).grid(row=7, column=1, sticky=tk.W)
-        self.clear = tk.Button(self.frame, text='Clear', command=self.clean_textbox, width=10).grid(row=7, column=1, sticky=tk.E)
+        self.create = tk.Button(self.frame, text='Create', command=self.create, width=10).grid(row=6, column=1,
+                                                                                               sticky=tk.W)
+        self.update = tk.Button(self.frame, text='Update', command=self.update, width=10).grid(row=6, column=1,
+                                                                                               sticky=tk.E)
+        self.delete = tk.Button(self.frame, text='Delete', command=self.delete, width=10).grid(row=7, column=1,
+                                                                                               sticky=tk.W)
+        self.clear = tk.Button(self.frame, text='Clear', command=self.clean_textbox, width=10).grid(row=7, column=1,
+                                                                                                    sticky=tk.E)
 
         # Tree widget
-        self.tree = ttk.Treeview(height=10, columns=("Code", "Name", "Phone", "City", "Email"), style="mystyle.Treeview")
+        self.tree = ttk.Treeview(height=10, columns=("Code", "Name", "Phone", "City", "Email"),
+                                 style="mystyle.Treeview")
         self.style = ttk.Style()
         self.style.configure("mystyle.Treeview", highlightthickness=0, bg=0)
         self.tree.grid(row=8, column=0, columnspan=2, pady=10, padx=10)
@@ -102,7 +108,7 @@ class MainApplication():
         """ Get all values from table and populate the tree widget"""
 
         self.clean_tree()
-        query = 'SELECT * FROM Inventario ORDER BY NOMBRE DESC'
+        query = 'SELECT * FROM Inventario ORDER BY ID DESC'
         db_rows = self.run_query(query)
         for row in db_rows:
             self.tree.insert('', 0, text=row[0], value=row[1:])
@@ -118,107 +124,77 @@ class MainApplication():
         self.email_var.set("")
         return
 
+    def check_parameters(self):
+        """Check if not empty fields"""
+
+        if len(self.name_var.get()) != 0 and len(self.phone_var.get()) != 0 and len(self.city_var.get()) != 0 \
+                and len(self.email_var.get()) != 0:
+            return True
+        return False
+
     def create(self):
+        """Insert value into db"""
+
         if len(self.code_var.get()) == 0:
-            if len(self.name_var.get()) != 0 and len(self.phone_var.get()) != 0 and len(
-                    self.city_var.get()) != 0 and len(self.email_var.get()) != 0:
+            if self.check_parameters():
                 query = 'INSERT INTO Inventario VALUES (NULL, ?, ?, ?, ?)'
-                parameters = (self.name_var.get()), (self.phone_var.get()), (self.city_var.get()), (self.email_var.get())
+                parameters = (self.name_var.get()), (self.phone_var.get()), (self.city_var.get()),\
+                             (self.email_var.get())
                 self.run_query(query, parameters)
                 self.clean_textbox()
                 self.get_contacts()
                 tk.messagebox.showinfo(title="OK", message="Created")
                 return
-
             else:
-                tk.messagebox.showerror(title="Error", message="Error")
+                tk.messagebox.showerror(title="Error", message="Error. Missing values")
                 return
-
         else:
-            tk.messagebox.showerror(title="Error", message="Unable to Create because tree item was selected."
+            tk.messagebox.showerror(title="Error", message="Unable to create because tree item was selected."
                                                            " Clean textbox and try again.")
             return
 
-    #
-    # def eliminar_contacto(self):
-    #     self.mensaje['text'] = ''
-    #     try:
-    #         self.tree.item(self.tree.selection())['text'][0]
-    #
-    #     except IndexError as e:
-    #         self.mensaje['text'] = 'Por favor selecciona un item'
-    #         return
-    #     self.mensaje['text'] = ''
-    #     name = self.tree.item(self.tree.selection())['text']
-    #     query = 'DELETE FROM Inventario WHERE NOMBRE = ?'
-    #     self.run_requery(query, (name,))
-    #     self.mensaje['text'] = 'Contacto {} eliminado satisfactoriamente'.format(name)
-    #     self.obtener_contacto()
-    #
-    # def editar_contacto(self):
-    #     self.mensaje['text'] = ''
-    #     try:
-    #         self.tree.item(self.tree.selection())['text'][0]
-    #
-    #     except IndexError as e:
-    #         self.mensaje['text'] = 'Por favor selecciona un item'
-    #         return
-    #     name = self.tree.item(self.tree.selection())['text']
-    #     telefono = self.tree.item(self.tree.selection())['values'][0]
-    #     ciudad = self.tree.item(self.tree.selection())['values'][1]
-    #     email = self.tree.item(self.tree.selection())['values'][2]
-    #     self.editar = Toplevel()
-    #     self.editar.title = 'Editar conacto'
-    #
-    #     # Editar Nombre
-    #     Label(self.editar, text='Nombre viejo:').grid(row=0, column=1)
-    #     Entry(self.editar, textvariable=StringVar(self.editar, value=name), state='readonly').grid(row=0, column=2)
-    #
-    #     Label(self.editar, text='Nuevo nombre:').grid(row=1, column=1)
-    #     new_name = Entry(self.editar)
-    #     new_name.grid(row=1, column=2)
-    #
-    #     # Editar Telefono
-    #     Label(self.editar, text='Telefono viejo:').grid(row=2, column=1)
-    #     Entry(self.editar, textvariable=StringVar(self.editar, value=telefono), state='readonly').grid(row=2, column=2)
-    #
-    #     Label(self.editar, text='Telefono nuevo:').grid(row=3, column=1)
-    #     new_telefono = Entry(self.editar)
-    #     new_telefono.grid(row=3, column=2)
-    #
-    #     # Editar Ciudad
-    #     Label(self.editar, text='Antigua ciudad:').grid(row=4, column=1)
-    #     Entry(self.editar, textvariable=StringVar(self.editar, value=ciudad), state='readonly').grid(row=4, column=2)
-    #
-    #     Label(self.editar, text='Ciudad nueva:').grid(row=5, column=1)
-    #     new_ciudad = Entry(self.editar)
-    #     new_ciudad.grid(row=5, column=2)
-    #
-    #     # Editar Email
-    #     Label(self.editar, text='Email viejo:').grid(row=6, column=1)
-    #     Entry(self.editar, textvariable=StringVar(self.editar, value=email), state='readonly').grid(row=6, column=2)
-    #
-    #     Label(self.editar, text='Nuevo email:').grid(row=7, column=1)
-    #     new_email = Entry(self.editar)
-    #     new_email.grid(row=7, column=2)
-    #
-    #     # Boton
-    #     Button(self.editar, text='Actualizar',
-    #            command=lambda: self.editar_item(new_name.get(), name, new_telefono.get(), telefono, new_ciudad.get(),
-    #                                             ciudad, new_email.get(), email)).grid(row=8, column=2, sticky=W)
-    #     self.editar.mainloop()
-    #
-    # def editar_item(self, new_name, name, new_telefono, telefono, new_ciudad, ciudad, new_email, email):
-    #     query = 'UPDATE Inventario SET Nombre = ?, Telefono = ?, Ciudad = ?, Email = ? WHERE NOMBRE = ? AND TELEFONO = ? AND CIUDAD = ? AND EMAIL = ?'
-    #     parameters = (new_name, new_telefono, new_ciudad, new_email, name, telefono, ciudad, email)
-    #     self.run_requery(query, parameters)
-    #     self.editar.destroy()
-    #     self.mensaje['text'] = 'Contacto {} actualizado correctamente.'.format(name)
-    #     self.obtener_contacto()
+    def update(self):
+        """update selected value"""
+
+        if len(self.code_var.get()) != 0:
+            if self.check_parameters():
+                query = 'UPDATE Inventario SET Nombre = ?, Telefono = ?, Ciudad = ?, Email = ? WHERE ID = ?'
+                parameters = (self.name_var.get()), (self.phone_var.get()), (self.city_var.get()), \
+                             (self.email_var.get()), (self.code_var.get())
+                self.run_query(query, parameters)
+                self.clean_textbox()
+                self.get_contacts()
+                tk.messagebox.showinfo(title="OK", message="Updated")
+                return
+            else:
+                tk.messagebox.showerror(title="Error", message="Some textbox may be left empty")
+                return
+        else:
+            tk.messagebox.showerror(title="Error", message="Unable to update because NO tree item was selected.")
+            return
+
+    def delete(self):
+        """delete selected value"""
+
+        if len(self.code_var.get()) != 0:
+            if self.check_parameters():
+                query = 'DELETE FROM Inventario WHERE ID =' + str(self.code_var.get())
+                self.run_query(query)
+                self.clean_textbox()
+                self.get_contacts()
+                tk.messagebox.showinfo(title="OK", message="Deleted")
+                return
+            else:
+                tk.messagebox.showerror(title="Error", message="Some textbox may be left empty")
+                return
+        else:
+            tk.messagebox.showerror(title="Error", message="Unable to delete because NO tree item was selected.")
+            return
 
 
 def main():
     """Create Main Window"""
+
     root = tk.Tk()
     MainApplication(root)
     root.mainloop()
